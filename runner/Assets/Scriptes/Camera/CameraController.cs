5c8a773398085c17b0f0d2d6ff70c367d864cf39
debosch <vlad.shiltsin@gmail.com>
2019-05-05 19:21:55 +0200

    Control added

---
+++ runner/Assets/Scriptes/Camera/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Transform target;
+    
+    private void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void Update()
+    {
+        Mathf.Clamp(transform.position.x, 0, Mathf.Infinity);
+
+        if (target != null)
+        {
+            transform.position = new Vector3(
+                target.position.x, 0, transform.position.z);
+        }
+    }
+}
---
+++ runner/Assets/Scriptes/Player/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundPoint;
+    [SerializeField] private float jumpForce;
+
+    private readonly float groundCheckRadius = 0.2f; 
+
+    private Animator animator;
+    private Rigidbody2D rb2D;
+
+    private bool isGrounded;
+   
+   private void Start()
+    { 
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundCheckRadius, groundMask);
+
+        LayerHolder();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        rb2D.AddForce(Vector2.up * jumpForce);
+        animator.SetTrigger("Jump");
+    }
+
+  
+    private void HorizontalMovement()
+    {
+
+    }
+
+    private void LayerHolder()
+    {
+        if (!isGrounded)
+        {
+            animator.SetLayerWeight(1, 1);
+            animator.SetLayerWeight(0, 0);
+        }
+        else
+        {
+            animator.SetLayerWeight(0, 1);
+            animator.SetLayerWeight(1, 0);
+        }
+            
+    }
+}
